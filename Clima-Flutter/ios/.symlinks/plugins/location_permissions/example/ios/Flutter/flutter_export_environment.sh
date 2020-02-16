086111276e21ea44e7cf728bd59ab2cda981e489
Kamal Heib <kamalheib1@gmail.com>
2020-02-16 23:39:28 +0200

    flutter-devel:  Clima-Flutter
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ Clima-Flutter/ios/.symlinks/plugins/location_permissions/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/maurits/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/maurits/Projects/Baseflow/Internal/Flutter/permission-plugins/packages/location_permissions/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/maurits/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ Clima-Flutter/lib/services/weather.dart
@@ -0,0 +1,65 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '8da7f21618d94bf2b7c98a0190843bc3';
+const theOpenWeatherManpUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$theOpenWeatherManpUrl?q=$cityName&'
+        'appid=$apiKey'
+        '&units=metric';
+
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        url: '$theOpenWeatherManpUrl?'
+            'lat=${location.latitude}&'
+            'lon=${location.longitude}&'
+            'appid=$apiKey'
+            '&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time in';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕 in';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤 in';
+    } else {
+      return 'Bring a 🧥 just in case in';
+    }
+  }
+}
